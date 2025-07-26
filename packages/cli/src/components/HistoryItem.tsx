10b40eee7b8f8690dd3539a11d4942333a4e8dd6
Harish <harish.16634@gmail.com>
2025-07-26 13:08:36 +0530

    small refactoring: refactoring HistoryItem.tsx to allow more message types

--- packages/cli/src/components/HistoryItem.tsx
+++ packages/cli/src/components/HistoryItem.tsx
@@ -8,22 +8,25 @@ export function HistoryItem({ item }: { item: Message | undefined }) {
 	}
 
 	return (
-		item.type === 'user' ? (
-			<Box borderColor={'blue'} borderStyle="round" flexDirection='row' paddingX={1}>
-				<Text>&gt; {" "}</Text>
-				<Text>{item.content}</Text>
-			</Box>
-		) : (
-			item.type === 'gemini' ? (
+		<>
+			{item.type === 'user' && (
+				<Box borderColor={'blue'} borderStyle="round" flexDirection='row' paddingX={1}>
+					<Text>&gt; {" "}</Text>
+					<Text>{item.content}</Text>
+				</Box>
+			)}
+			{item.type === 'gemini' && (
 				<Box marginLeft={1} marginBottom={1} gap={1}>
 					<Text color="green"> {"✦"}</Text>
 					<Text>{item.content}</Text>
 				</Box>
-			) : (
+			)}
+			{item.type === 'system' && (
 				<Box marginLeft={1} marginBottom={1} gap={1}>
-					<Text color="red">{item.content}</Text>
+					<Text color="red">{"✦"}</Text>
+					<Text>{item.content}</Text>
 				</Box>
-			)
-		)
+			)}
+		</>
 	)
 }
